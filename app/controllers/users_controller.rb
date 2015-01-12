2122af7e03912ac0084952c31c4003a89337179e
jiemoon <jiemoon2010@163.com>
2015-01-12 22:30:26 +0800

    form valid, i18n, flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,48 @@
 class UsersController < ApplicationController
-  def signup
-  	@user = User.new
-  end
+	def signup
+		@user = User.new
+	end
 
-  def create
-  	# render plain: params[:user].inspect
-  	user = User.create(user_params)
-  	user.save
-  	cookies[:auth_token] = user.auth_token
-  	redirect_to :root
-  end
+	def create
+		# render plain: params[:user].inspect
+		@user = User.create(user_params)
+		if @user.save
+			cookies[:auth_token] = @user.auth_token
+			redirect_to :root
+		else
+			render :signup
+		end
+	end
 
-  def login
+	def login
 
-  end
+	end
 
-  def create_login_session
-  	user = User.find_by_name(params[:name])
-  	if user && user.authenticate(params[:password])
-  		# session[:user_id] = user.id
-  		if params[:remember_me]
-  			cookies.permanent[:auth_token] = user.auth_token
-  		else
-  			cookies[:auth_token] = user.auth_token
-  		end
-  		redirect_to :root
-  	else
-  		redirect_to :login
-  	end
-  end
+	def create_login_session
+		user = User.find_by_name(params[:name])
+		if user && user.authenticate(params[:password])
+			# session[:user_id] = user.id
+			if params[:remember_me]
+				cookies.permanent[:auth_token] = user.auth_token
+			else
+				cookies[:auth_token] = user.auth_token
+			end
+			flash.notice = "登陆成功！"
+			redirect_to :root
+		else
+			flash.notice = "用户名密码错误!"
+			redirect_to :login
+		end
+	end
 
-  def logout
-  	# session[:user_id] = nil
-  	cookies.delete(:auth_token)
-  	redirect_to :root
-  end
+	def logout
+		# session[:user_id] = nil
+		cookies.delete(:auth_token)
+		redirect_to :root
+	end
 
-  private
-  def user_params
-  	params.require(:user).permit!
-  end
+	private
+	def user_params
+		params.require(:user).permit!
+	end
 end
--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module Meetup
         g.helper false
         g.test_framework false
     end
+
+    config.i18n.default_locale = 'zh-CN'
   end
 end
